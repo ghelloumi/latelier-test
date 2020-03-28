3550d59f30a927ac7d4337fbbae52e70acbc7059
Ghassen <ell.ghassen@gmail.com>
2020-03-28 15:41:21 +0100

    add SASS support

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -2,6 +2,9 @@ import {createLogger} from 'redux-logger'
 import {createStore, applyMiddleware} from 'redux'
 import reducers from './reducers'
 import headToHeadData from '../data/headtohead.json';
+import {fetchPlayers} from "../players.service";
+
+fetchPlayers().then(e => console.log(e))
 
 const initialState = {data: JSON.parse(JSON.stringify(headToHeadData))}
 
--- src/players.service.js
+++ src/players.service.js
@@ -1,15 +1,15 @@
-// import {URI} from "./utils/constants"
+import {URI} from "./utils/constants"
 
-// export const fetchPlayers = async () => {
-//     try {
-//         const res = await fetch(URI, {
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'Access-Control-Allow-Origin': 'http://localhost:3000'
-//             },
-//         })
-//         return res.json()
-//     } catch (e) {
-//         console.error(e)
-//     }
-// }
+export const fetchPlayers = async () => {
+    try {
+        const res = await fetch(URI, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': 'http://localhost:3000'
+            },
+        })
+        return res.json()
+    } catch (e) {
+        console.error(e)
+    }
+}
